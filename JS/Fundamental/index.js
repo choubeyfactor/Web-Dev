b26b5da16b55bbc43516ac5f5104343ea43bb6c1
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-12 22:29:50 +0530

    Add files via upload

---
+++ JS/Fundamental/index.js
@@ -0,0 +1,31 @@
+//line below is used to print on the console
+console.log('Namste Duniya aversion 4');
+
+let a = true ;
+console.log(a);
+let name = "ziaa";
+console.log(name);
+
+
+let arr = [1,2,"sanket",false];//defining an array
+console.log(arr);
+arr[0] = arr[3]; //changing the variable's value
+console.log(arr);
+
+
+//--------------------OPERATOR------------------------//
+
+
+let x = 2 ;
+++x; //pre-increament
+console.log(x);
+x++; //post-increament
+console.log(x++);
+
+
+//--------------------LOOP----------------------------//
+
+let i = 0;
+do{
+i++;
+}while (condition);
