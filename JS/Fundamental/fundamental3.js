3e130826f9c13ee2bc8c21a6c6eae101a8c8f88e
MAYANK CHOUBEY <98317830+choubeyfactor@users.noreply.github.com>
2023-10-21 23:54:59 +0530

    Add files via upload

---
+++ JS/Fundamental/fundamental3.js
@@ -0,0 +1,22 @@
+let x = Math.random;
+Math.round(1.2);
+Math.max(2,1,3,4);
+Math.min(2,1,3,4);
+Math.pow(2,3);
+Math.abs(2.8);
+Math.sqrt(9);
+
+//----------String--------------//
+
+//This below thing is string
+let firstName = "Mayank";
+//this below thing acts as an object
+let lastName = new String("Ziaa");
+
+lastName.startsWith();
+lastName.endsWith();
+firstName.toLocaleUpperCase();
+lastName.toLocaleLowerCase();
+lastName.trim(); //to remove spaces
+lastName.replace('z','a'); //will replace z to a
+
